77547b118b23727e2fabe43267bb3577ae4c8247
Gabriel Chaves <gabrielccavalcante@gmail.com>
2025-08-11 01:35:17 -0300

    switch_case.js

---
+++ switch_case.js
@@ -0,0 +1,16 @@
+let preço = 40
+switch (preço){
+    case 20:
+         console.log("caso1")
+    break
+    case 25:
+        console.log("caso2")
+    break
+    case 30:
+        console.log("caso3 ")
+    break    
+    case 40:
+        console.log("caso4")
+    break
+
+}
